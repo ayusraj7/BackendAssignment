2d41110c14d2b7e391f9acfe2e353332879cbbb2
ayusraj7 <107795694+ayusraj7@users.noreply.github.com>
2024-03-08 11:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mongoose=require('mongoose')
 
 //database connection 
 const connectwithDb=()=>{
-    mongoose.connect(process.env.DATABASE_URL,{})
+    mongoose.connect(process.env.MONGODB_URL,{})
     .then(console.log('DB connected successfully'))
     .catch((error)=>{
         console.log('DB facing connection issues ')
